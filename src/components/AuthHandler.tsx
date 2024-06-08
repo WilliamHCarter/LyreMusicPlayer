200978fdd95a5b89ab90aa1e789921e9ca2d5e37
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2024-06-07 23:58:26 -0400

    refactor authhandler

--- src/components/AuthHandler.tsx
+++ src/components/AuthHandler.tsx
@@ -4,36 +4,42 @@ import { LoaderCircle } from "lucide-solid";
 
 export const [isAuthorizing, setIsAuthorizing] = createSignal(false);
 export const [token, setToken] = createSignal("");
+import { createMutation } from '@tanstack/solid-query';
+
 const AuthHandler = () => {
+  const [isAuthorizing, setIsAuthorizing] = createSignal(false);
+
+  const exchangeCodeForTokenMutation = createMutation(exchangeCodeForToken);
+
   onMount(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
     const state = urlParams.get("state");
     const storedState = localStorage.getItem("spotifyAuthState");
     if (code && state && state === storedState) {
-        exchangeCodeForToken(code);
+      exchangeCodeForTokenMutation.mutate(code);
     }
   });
 
-createEffect(() => {
+  createEffect(() => {
     if (isAuthorizing()) {
-        console.log("Authorizing...");
-        login();
-        setIsAuthorizing(false);
+      console.log("Authorizing...");
+      login();
+      setIsAuthorizing(false);
     } else {
-        console.log("Not Authorizing...");
+      console.log("Not Authorizing...");
     }
-}, isAuthorizing());
+  }, isAuthorizing());
 
   return (
     <>
       <div
         class={`absolute flex z-99 top-0 justify-center items-center w-full h-full bg-black bg-opacity-50`}
-            style={{ visibility: isAuthorizing() ? "visible" : "hidden" }}
-        >
+        style={{ visibility: exchangeCodeForTokenMutation.isLoading ? "visible" : "hidden" }}
+      >
         <div class="flex flex-col items-center gap-2 bg-slate-700 p-6 rounded-lg">
           <p class="text-white text-xl">Authorizing...</p>
-          <LoaderCircle class="animate-spin" color="white"/>
+          <LoaderCircle class="animate-spin" color="white" />
         </div>
       </div>
     </>
